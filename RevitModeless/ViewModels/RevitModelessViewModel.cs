591bff910dda8e57662c07faf5266fc3a49a9e7c
Kate <nemkovich.kate@gmail.com>
2022-10-25 23:19:01 +0300

    With DeleteHandler

--- RevitModeless/ViewModels/RevitModelessViewModel.cs
+++ RevitModeless/ViewModels/RevitModelessViewModel.cs
@@ -2,27 +2,25 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Autodesk.Revit.DB;
+using RevitModeless.Core;
 
 namespace RevitModeless.ViewModels;
 
 public sealed class RevitModelessViewModel : ObservableObject
 {
-    private readonly UIDocument _uiDocument;
+    private readonly ExternalEvent _externalEvent;
+
     public RelayCommand DeleteCommand { get; }
 
     private void Delete()
     {
-        var document = _uiDocument.Document;
-        using var transaction = new Transaction(document);
-        transaction.Start("Delete elements");
-        //Когда получаем то, что уже выделено
-        document.Delete(_uiDocument.Selection.GetElementIds());
-        transaction.Commit();
+     _externalEvent.Raise();
+     Console.WriteLine();
     }
 
     public RevitModelessViewModel(UIDocument uiDocument)
     {
-        _uiDocument = uiDocument;
         DeleteCommand = new RelayCommand(Delete);
+        _externalEvent = ExternalEvent.Create(new DeleteHandler());
     }
 }

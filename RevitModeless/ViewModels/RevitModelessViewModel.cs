5c0bdd36cd4e1d3d8f179792a2f72f2ac33b5951
Kate <nemkovich.kate@gmail.com>
2022-10-26 22:00:49 +0300

    With one file and ActionEventHandler

--- RevitModeless/ViewModels/RevitModelessViewModel.cs
+++ RevitModeless/ViewModels/RevitModelessViewModel.cs
@@ -2,25 +2,34 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Autodesk.Revit.DB;
+using Nice3point.Revit.Toolkit.External;
+using Nice3point.Revit.Toolkit.External.Handlers;
 using RevitModeless.Core;
 
 namespace RevitModeless.ViewModels;
 
 public sealed class RevitModelessViewModel : ObservableObject
 {
-    private readonly ExternalEvent _externalEvent;
+    private readonly ActionEventHandler _externalEvent;
 
     public RelayCommand DeleteCommand { get; }
 
     private void Delete()
     {
-     _externalEvent.Raise();
-     Console.WriteLine();
+        _externalEvent.Raise(application =>
+        {
+            var uiDocument = application.ActiveUIDocument;
+            var document = uiDocument.Document;
+            using var transaction = new Transaction(document);
+            transaction.Start("Delete elements");
+            //Когда получаем то, что уже выделено
+            document.Delete(uiDocument.Selection.GetElementIds());
+            transaction.Commit();
+        });
     }
-
     public RevitModelessViewModel(UIDocument uiDocument)
     {
         DeleteCommand = new RelayCommand(Delete);
-        _externalEvent = ExternalEvent.Create(new DeleteHandler());
+        _externalEvent = new ActionEventHandler();
     }
 }
